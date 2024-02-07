95138d313858afb339266d3b95fbfd537f53f9eb
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2024-02-07 09:36:49 +0000

    DPR2-46: Remove unused and ported lambdas

--- src/main/java/uk/gov/justice/digital/clients/glue/GlueProvider.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.clients.glue;
-
-import com.amazonaws.services.glue.AWSGlue;
-
-public interface GlueProvider {
-    AWSGlue buildClient();
-}
--- src/main/java/uk/gov/justice/digital/clients/s3/S3Provider.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.clients.s3;
-
-import com.amazonaws.services.s3.AmazonS3;
-
-public interface S3Provider {
-    AmazonS3 buildClient();
-}
