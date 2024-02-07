f85cd206f4f2e0f10b4ea05a08f9278ba6d277ae
koladeadewuyi-moj <136330532+koladeadewuyi-moj@users.noreply.github.com>
2024-02-07 17:40:35 +0000

    DPR2-46: Remove unused and ported lambdas (#11)

--- src/main/java/uk/gov/justice/digital/common/Utils.java
+++ src/main/java/uk/gov/justice/digital/common/Utils.java
@@ -9,21 +9,20 @@ public class Utils {
 
     public final static String DEFAULT_DPR_REGION = "eu-west-2";
     public final static String TASK_TOKEN_KEY = "token";
+    public final static String TOKEN_EXPIRY_DAYS_KEY = "tokenExpiryDays";
+    public final static long DEFAULT_TOKEN_EXPIRY_DAYS = 7;
     public final static String REPLICATION_TASK_ARN_KEY = "replicationTaskArn";
-    public final static String DELIMITER = "/";
-    public final static String FILE_EXTENSION = ".parquet";
-    public final static String CONFIG_PATH = "configs/";
-    public final static String CONFIG_FILE_SUFFIX = "/table-config.json";
 
     public static Optional<String> getOptionalString(Map<String, Object> event, String key) {
         return Optional.ofNullable(event.get(key)).map(obj -> (String) obj);
     }
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"unchecked", "unused"})
     public static Map<String, String> getConfig(Map<String, Object> event, String key) {
         return (Map<String, String>) event.getOrDefault(key, Collections.emptyMap());
     }
 
+    @SuppressWarnings({"unused"})
     public static <T, O> T getOrThrow(Map<String, O> obj, String key, Class<T> type) {
         return Optional.ofNullable(type.cast(obj.get(key)))
                 .orElseThrow(() -> new NoSuchElementException("Required key [" + key + "] is missing"));
